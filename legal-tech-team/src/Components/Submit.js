ae29227f55737fa132bdcb5a438cd93963c300d4
Nylan Pierre <nylanp10@gmail.com>
2024-04-17 11:12:24 -0400

    I got patching the word document to work with the api call and removed secret

--- legal-tech-team/src/Components/Submit.js
+++ legal-tech-team/src/Components/Submit.js
@@ -12,6 +12,7 @@ import { saveAs } from 'file-saver';
 import { generateReport, summarizeFile  } from "../HelperFunctions/apiCalls";
 import JSZip from 'jszip';
 import * as pdfjs from 'pdfjs-dist/build/pdf.min.mjs';
+import { handleTemplateInput } from "../HelperFunctions/GenerateWordDocument";
 await import('pdfjs-dist/build/pdf.worker.min.mjs');
 
 function Submit() {
@@ -20,6 +21,7 @@ function Submit() {
   const [submitSuccess, setSubmitSuccess] = useState(false);
   const [wifiConnected, setWifiConnected] = useState(false);
   const fileInputRef = React.useRef(null); 
+  const [callSuccess, setCallSuccess] = useState(false);
 
 
   const handleFileChange = (event) => {
@@ -50,6 +52,7 @@ function Submit() {
   
     // Set the selected file to state
     setFile(selectedFile);
+    setCallSuccess(true);
   };
 
   const handleSubmit = () => {
@@ -173,6 +176,7 @@ function Submit() {
               style={{ display: "none" }} // Hide the file input
               onChange={handleFileChange_summarize}
             />
+            
 
             {wifiConnected && !submitSuccess && (
               <Box sx={{ marginTop: "20px" }}>
@@ -180,6 +184,12 @@ function Submit() {
                 <Button variant="contained" onClick={handleSubmit} style={{ marginLeft: "10px", marginTop: "10px" }}>
                   Submit
                 </Button>
+              </Box>
+            )}
+
+            {wifiConnected && !submitSuccess && callSuccess && (
+              <Box sx={{ marginTop: "20px" }}>
+                <input type="file" onChange={handleTemplateInput} />
               </Box>
             )}
 
